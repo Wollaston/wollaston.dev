f9043bd314ad48591a6f70f44034ce5131e841b6
Wollaston <mattflynn1997@gmail.com>
2024-01-23 13:41:36 -0500

    Update text

--- src/app/routes/projects/aratype.rs
+++ src/app/routes/projects/aratype.rs
@@ -44,7 +44,7 @@ pub fn Converter() -> impl IntoView {
                         </div>
                     </div>
                     <div class="flex flex-1 w-full p-2 flex-col">
-                        <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">"Buckwalter Conversion: "</p>
+                        <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">"Buckwalter Transliteration: "</p>
                         <div class="flex-1 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" dir="rtl">{converted}</div>
                     </div>
                 </div>
