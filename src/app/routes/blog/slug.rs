a0c369f5d6eb3089e7841d0c450e93aa8b29777f
Wollaston <mattflynn1997@gmail.com>
2024-01-22 23:21:51 -0500

    Move markdown parsing to server function

--- src/app/routes/blog/slug.rs
+++ src/app/routes/blog/slug.rs
@@ -3,7 +3,7 @@ use gray_matter::Matter;
 use leptos::*;
 use leptos_router::{use_params, Params};
 use pulldown_cmark::{html, Options, Parser};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Params, Clone, PartialEq, Eq, Debug)]
 struct BlogParams {
@@ -14,24 +14,10 @@ struct BlogParams {
 pub fn Slug() -> impl IntoView {
     let params = use_params::<BlogParams>();
 
-    let raw_content = create_resource(
+    let content = create_resource(
         move || params.get(),
         move |params| async move { get_post(params.unwrap().slug).await },
     );
-    // leptos::logging::log!("{:?}", raw_content);
-    // let content = raw_content.get().unwrap().unwrap();
-    // let content = content.as_str();
-    //
-    // let matter = Matter::<YAML>::new();
-    // let front_matter = matter.parse_with_struct::<FrontMatter>(content).unwrap();
-    //
-    // let mut options = Options::empty();
-    // options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
-    //
-    // let parser = Parser::new_ext(&front_matter.content, options);
-    //
-    // let mut html_ouput = String::new();
-    // html::push_html(&mut html_ouput, parser);
 
     view! {
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 dark:bg-gray-900 antialiased">
@@ -40,12 +26,20 @@ pub fn Slug() -> impl IntoView {
                 <Suspense
                 fallback=move || view! {<p>"Loading..."</p> }
                 >
-                    <header class="mb-4 lg:mb-6 not-format">
-                        <h1 class="mb-4 text-3xl font-extrabold leading-tight text-blue-700 lg:mb-6 lg:text-4xl dark:text-white">"TITLE"</h1>
-                    </header>
-                    {move || match raw_content.get() {
+                    {move || match content.get() {
                         None => view! {<h1>"Error Loading Content"</h1>}.into_any(),
-                        Some(raw_content) => view! {<h1>{raw_content}</h1>}.into_any(),
+                        Some(content) => match content {
+                            Ok(content) => view! {
+                            <div>
+                                <header class="mb-4 lg:mb-6 not-format">
+                                    <h1 class="mb-4 text-3xl font-extrabold leading-tight text-blue-700 lg:mb-6 lg:text-4xl dark:text-white">{content.front_matter.title}</h1>
+                                </header>
+                                <div inner_html=content.content />
+                                // <h1 class="md-h1">"Test Header"</h1>
+                            </div>
+                        }.into_any(),
+                            Err(_) => view! {<h1>"Error Loading Content"</h1>}.into_any(),
+                        }
                     }}
                 </Suspense>
             </article>
@@ -54,19 +48,39 @@ pub fn Slug() -> impl IntoView {
     }
 }
 
-#[derive(Debug, Deserialize)]
-struct FrontMatter {
-    date: String,
-    description: String,
-    slug: String,
-    title: String,
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct FrontMatter {
+    pub date: String,
+    pub description: String,
+    pub slug: String,
+    pub title: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Content {
+    pub front_matter: FrontMatter,
+    pub content: String,
 }
 
 #[server(GetPost, "/api")]
-pub async fn get_post(slug: String) -> Result<String, ServerFnError> {
+pub async fn get_post(slug: String) -> Result<Content, ServerFnError> {
     let res = reqwest::get(format!("http://127.0.0.1:3000/assets/blog/{slug}.md")).await?;
-    leptos::logging::log!("{:#?}", res);
     let content = res.text().await?;
-    leptos::logging::log!("{:#?}", content);
-    Ok(content)
+    let content = content.as_str();
+
+    let matter = Matter::<YAML>::new();
+    let front_matter = matter.parse_with_struct::<FrontMatter>(content).unwrap();
+
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+
+    let parser = Parser::new_ext(&front_matter.content, options);
+
+    let mut html_ouput = String::new();
+    html::push_html(&mut html_ouput, parser);
+
+    Ok(Content {
+        front_matter: front_matter.data,
+        content: html_ouput,
+    })
 }
