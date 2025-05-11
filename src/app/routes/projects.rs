71a5f0ba02b5f31530302a8eea5bf8c7e9d41c82
Wollaston <matt@wollaston.dev>
2025-05-11 11:14:11 -0400

    Add conditional GitHub link to projects

--- src/app/routes/projects.rs
+++ src/app/routes/projects.rs
@@ -31,7 +31,7 @@ fn Projects() -> impl IntoView {
                                         Either::Left(view! { <p>"No projects were found."</p> }.into_any())
                                     } else {
                                         Either::Right(projects.into_iter().map(move |project| {
-                                            view! {<li class="p-2 m-2 font-semibold text-3xl text-stone-800 hover:text-blue-700 hover:underline dark:text-stone-100 dark:hover:text-[#fd8a04]"><a href={project.path}>{project.title.to_uppercase()}</a></li>}
+                                            view! {<ProjectItem project=project />}
                                         }).collect_view())
                                     }
                                 })
@@ -41,6 +41,28 @@ fn Projects() -> impl IntoView {
                 </Transition>
             </ul>
         </div>
+    }
+}
+
+#[component]
+pub fn ProjectItem(project: Project) -> impl IntoView {
+    let (project, _) = signal(project);
+    let has_github_link = move || project.get().github_link.is_some();
+
+    view! {
+        <p>
+            {move || match has_github_link() {
+                true => {
+                    view! {<li class="p-2 m-2 "><a target="_blank" rel="noopener noreferrer" class="font-semibold text-3xl text-stone-800 hover:text-blue-700 hover:underline dark:text-stone-100 dark:hover:text-[#fd8a04]" href={project.get().path}>{project.get().title.to_uppercase()}</a>
+                            <a href={project.get().github_link} target="_blank" rel="noopener noreferrer" class="inline-flex items-center text-blue-700 dark:text-stone-100 hover:underline">
+                                <img src="/github-mark.svg" alt="The GitHub Logo" class="block dark:hidden hover:shadow-2xl shadow-blue-700 h-6 w-6 m-2"/>
+                                <img src="/github-mark-white.svg" alt="The GitHub Logo" class="hidden dark:block hover:shadow-2xl shadow-blue-700 h-6 w-6 m-2"/>
+                            </a>
+                    </li>}.into_any()
+                },
+                _ => view! {<li class="p-2 m-2 "><a target="_blank" rel="noopener noreferrer" class="font-semibold text-3xl text-stone-800 hover:text-blue-700 hover:underline dark:text-stone-100 dark:hover:text-[#fd8a04]" href={project.get().path}>{project.get().title.to_uppercase()}</a></li>}.into_any()
+            }}
+        </p>
     }
 }
 
