44500adf1db7c6f9871d73bad109db7591722067
Wollaston <matt@wollaston.dev>
2025-05-11 08:18:26 -0400

    Remove sqlite migrations

--- migrations/20240128180734_projects.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS projects (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    title       VARCHAR(250)        NOT NULL,
-    path        VARCHAR(250)        NOT NULL,
-    github_link VARCHAR(250)        NOT NULL,
-    description VARCHAR(250)        NOT NULL
-);
